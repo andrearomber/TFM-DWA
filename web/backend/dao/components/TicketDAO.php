ebdaf09393a7aafcfb4341dca7e549b4e3ec0cde
andrearomber <andrea.romero@grupoadd.es>
2024-09-11 06:57:44 +0200

    All DAOs files

---
+++ web/backend/dao/components/TicketDAO.php
@@ -0,0 +1,37 @@
+<?php
+require_once __DIR__ . '/../BaseDAO.php';
+
+class TicketDAO extends BaseDAO {
+    public function __construct() {
+        parent::__construct('tickets');
+    }
+
+    public function getItemById($id) {
+        $stmt = $this->db->prepare("SELECT * FROM {$this->table} WHERE ticket_id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function updateItem($id, $data) {
+        $set = implode(", ", array_map(function($key) {
+            return "$key = :$key";
+        }, array_keys($data)));
+
+        $sql = "UPDATE {$this->table} SET $set WHERE ticket_id = :id";
+        $stmt = $this->db->prepare($sql);
+
+        foreach ($data as $key => $value) {
+            $stmt->bindValue(":$key", $value);
+        }
+
+        $stmt->bindValue(":id", $id);
+        return $stmt->execute();
+    }
+
+    public function deleteItem($id) {
+        $stmt = $this->db->prepare("DELETE FROM {$this->table} WHERE ticket_id = :id");
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+    }
+}
